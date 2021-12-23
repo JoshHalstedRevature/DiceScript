f56de513ade0fc32ccaf4e0bc87ed108c8a65559
pstapleton1925 <92169330+pstapleton1925@users.noreply.github.com>
2021-12-23 20:08:30 +0000

    add readme and app.log update main

---
+++ README.md
@@ -0,0 +1,24 @@
+Dice roll simulator
+
+# Description
+
+This project is a simple dice rolling application that rolls two dice with a user-specified number of sides. The user can choose between 6, 8, 10, 12, 20, and 100-sided dice.
+
+# Libraries, modules, and tools
+
+This project does not use any external libraries. The Python modules used in this project are random and logging. Pylint was used for code analysis.
+
+# Project requirements:
+-Create a GitHub Repo
+
+-Readme File - Brief desc abt project, Libraries used etc.,
+
+-Codes (Incl. Logging Module )
+
+-Output snapshot
+
+-Use of Pylint - code analysis for Python
+
+-Unit testing
+
+### bonus: architecture mapping in readme
--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 #probably has to do with the function, maybe just fixing the return for it or something 
 
-#max is a keyword, so I changed it to Sides. So is min, so it was removed.
 #pog
 
 import random
 import logging
-import os
+#import os
+
+logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 
 def printDice(num):
-  print("-----------")
+  """Working on this (Displays every pip as of now)..."""
+  halfNum = int(num/2)
+  print(f"%s"%"_ "*(num))
+  for n in range(0, halfNum, 1):
+    print("| ", "• "*halfNum, "|")
+  print("¯ "*num)
 
 
 def MaxChoice():
   """Will decide the ..."""
-  curious = input("What dice size would you like? (6/8/10/12/20/100)")
+  curious = input("What dice size would you like? (6/8/10/12/20/100): ")
+  curious = int(curious)
+  Sides = 0
   if curious == 6:
     Sides = 6
   elif curious == 8:
@@ -24,27 +32,25 @@ def MaxChoice():
     Sides = 12 
   elif curious == 20:
     Sides = 20
-  else:
+  elif curious == 100:
     Sides = 100
+  printDice(Sides) # Testing
   return Sides
 
 DiceCheck = "yes"
-numSides = 0
+NumSides = 20
 if DiceCheck == "yes" or DiceCheck == "y" or DiceCheck == "Yes" or DiceCheck == "Y":
   NumSides = MaxChoice()
-
 check = "yes"
 
-# Are the results supposed to be 86 and 35?
-
 while check == "yes" or check == "y" or check == "Yes" or check == "Y":
-    print("Rolling the dices...")
+    print("Rolling the dice...")
     print("The values are....")
-    print(round(random.random()*numSides + 1))
-    print(round(random.random()*numSides + 1))
+    print(round(random.random()*NumSides + 1))
+    print(round(random.random()*NumSides + 1))
     
 
-    check = input("Roll the dices again?:")
+    check = input("Roll the dice again?:")
     if check == "yes" or check == "y" or check == "Yes" or check == "Y":
       DiceCheck = input("Would you like to select different dice?:")
       if DiceCheck == "yes" or DiceCheck == "y" or DiceCheck == "Yes" or DiceCheck == "Y":
