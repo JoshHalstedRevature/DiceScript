3a2337cd2e2d152f4030f58c7258570f1ef9e49a
pstapleton1925 <92169330+pstapleton1925@users.noreply.github.com>
2021-12-24 21:25:00 +0000

    add logging; split functions

--- main.py
+++ main.py
@@ -1,64 +1,47 @@
-#probably has to do with the function, maybe just fixing the return for it or something 
-
-#pog
-
 import random
 import logging
-#import os
 
-logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename='app.log', filemode='w', level=logging.DEBUG, format='%(asctime)s %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S')
+logging.info("Logging activity for DiceRoll...")
 
-def printDice(num):
-  """Working on this (Displays every pip as of now)..."""
-  halfNum = int(num/2)
-  print(f"%s"%"_ "*(num))
-  for n in range(0, halfNum, 1):
-    print("| ", "• "*halfNum, "|")
-  print("¯ "*num)
+def getSidesInt():
+    tries = 0
+    logging.info("Requesting input for # of sides per die")
+    while tries < 3:
+        curious = input("We're rolling two dice.\nWhat size would you like? (no. of sides): ")
+        try:
+            int(curious)
+        except ValueError:
+            tries += 1
+            logging.error(f"Invalid input = {curious}; attempts: {tries}/3")
+            print(f"Input invalid (attempts: {tries}/3)")
+        else:
+            logging.info(f"Valid input = {curious}")
+            print(f"Okay, we're rolling two d{curious}s")
+            return int(curious)
 
+def rollDice(NumSides):
+    try:
+        dieA = round(random.random()*NumSides + 1)
+        dieB = round(random.random()*NumSides + 1)
+        #logging.debug(f"dieA * 5 = {dieA * 5}")
+    except TypeError:
+        logging.error(f"NumSides is not a number: NumSides = {NumSides}")
+    else:
+        logging.info("Rolling dice...")
+        print(f"The values are...\n{dieA}\n{dieB}")
 
-def MaxChoice():
-  """Will decide the ..."""
-  curious = input("What dice size would you like? (6/8/10/12/20/100): ")
-  curious = int(curious)
-  Sides = 0
-  if curious == 6:
-    Sides = 6
-  elif curious == 8:
-    Sides = 8
-  elif curious == 10:
-    Sides = 10 
-  elif curious == 12:
-    Sides = 12 
-  elif curious == 20:
-    Sides = 20
-  elif curious == 100:
-    Sides = 100
-  printDice(Sides) # Testing
-  return Sides
-
-DiceCheck = "yes"
-NumSides = 20
-if DiceCheck == "yes" or DiceCheck == "y" or DiceCheck == "Yes" or DiceCheck == "Y":
-  NumSides = MaxChoice()
 check = "yes"
-
+diceCheck = "yes"  
+numSides = getSidesInt()
 while check == "yes" or check == "y" or check == "Yes" or check == "Y":
-    print("Rolling the dice...")
-    print("The values are....")
-    print(round(random.random()*NumSides + 1))
-    print(round(random.random()*NumSides + 1))
-    
-
+    rollDice(numSides)
     check = input("Roll the dice again?:")
     if check == "yes" or check == "y" or check == "Yes" or check == "Y":
-      DiceCheck = input("Would you like to select different dice?:")
-      if DiceCheck == "yes" or DiceCheck == "y" or DiceCheck == "Yes" or DiceCheck == "Y":
-        NumSides = MaxChoice()
-
-
-def main():
-  pass
-        
-if __name__ == "__main__":
-  main()
+        logging.info("User chose to reroll")
+        diceCheck = input("Would you like to select different dice?:")
+        if diceCheck == "yes" or diceCheck == "y" or diceCheck == "Yes" or diceCheck == "Y":
+            logging.info("User wants new dice")
+            numSides = getSidesInt()
+    else:
+        logging.info("User chose not to reroll")
